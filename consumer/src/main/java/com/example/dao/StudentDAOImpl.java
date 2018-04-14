8bfe9953cf98ee354cd2623d5e4ee4cf748e2e88
ibnvakbar <r.ibnuakbar@gmail.com>
2018-04-14 23:11:23 +0700

    updated

--- consumer/src/main/java/com/example/dao/StudentDAOImpl.java
+++ consumer/src/main/java/com/example/dao/StudentDAOImpl.java
@@ -1,9 +1,11 @@
 package com.example.dao;
 
 import com.example.model.StudentModel;
+import com.sun.tools.javac.code.Attribute;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class StudentDAOImpl implements StudentDAO {
@@ -19,7 +21,7 @@ public class StudentDAOImpl implements StudentDAO {
 
     @Override
     public List<StudentModel> selectAllStudents() {
-        StudentModel student = restTemplate.getForObject("http://localhost:8181/rest/student/viewall", StudentModel.class);
-        return (List<StudentModel>) student;
+        StudentModel[] student = restTemplate.getForObject("http://localhost:8181/rest/student/viewall", StudentModel[].class);
+        return Arrays.asList(student);
     }
 }
